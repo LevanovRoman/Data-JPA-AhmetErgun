40665359cd4907e071e9a1b22de16abef1fdce9e
levanovroman <levanovroman2016@yandex.ru>
2024-06-10 09:24:21 +0300

    finish

--- src/main/java/com/myapp/data_jpa_course_AhmetErgun/model/Author.java
+++ src/main/java/com/myapp/data_jpa_course_AhmetErgun/model/Author.java
@@ -23,7 +23,7 @@ public class Author {
     @JoinColumn(name = "zipcode_id")
     private Zipcode zipcode;
 
-    @ManyToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToMany(mappedBy = "authors", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Book> books = new ArrayList<>();
 
 //    ------
